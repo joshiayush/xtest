897837339dd10cd173352d87621816acd3020cd1
Ayush Joshi <ayush854032@gmail.com>
2021-12-01 13:50:38 +0530

    feat(assertions): use `EXPECT_*` and `ASSERT_*` assertion macros when writing a `test suite`
    use `EXPECT_*` when you want the test to reveal more erros after the assertion failure,
    and use `ASSERT_*` when continuing after failure doesn't make sense
    
    define `IMPL__TEST_EQ` and `IMPL__TEST_NE` macro to do a equality and non-equality check
    respectively and in case the evaluation of the expression produce a different result then
    call operations defined in `IMPL__TEST_FAILED` macro to decide whether to abort the current
    process or just simply continue incrementing the `xtest::g_n_test_failures` global variable
    
    define `IMPL__TEST_TRUE` and `IMPL__TEST_FALSE` to check for the `truthiness` and `falsiness`
    of the given expressions respectively and in case of failure call operations defined in
    macro `IMPL__TEST_FAILED` to decide whether to abort the current process or just simply
    continue incrementing the `xtest::g_n_test_failures` global varaible
    Note: The given expressions can be anything that evaluates to `true` or `false` when used
    inside of a `if` clause so be sure before giving a expression to this macro

---
+++ xtest/xtext.cc
@@ -0,0 +1,29 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include <cinttypes>
+
+#include "xtest.hh"
+
+namespace xtest {
+/**
+ * @brief Global counter for non-fatal test failures.
+ *
+ * This global counter is defined in the object file xtest.cc and incremented
+ * every time a non-fatal test assertion fails.
+ */
+uint64_t g_n_test_failures = 0;
+}  // namespace xtest
