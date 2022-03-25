3efe5b95633b7c0ded5f42fb55924229487186a0
Ayush Joshi <ayush854032@gmail.com>
2022-03-25 10:07:19 +0530

    docs(printers): document function `PrintColorEncoded()`
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- include/internal/xtest-printers.hh
+++ include/internal/xtest-printers.hh
@@ -48,6 +48,14 @@ bool ShouldUseColor();
 // redirected to a file then we disable color output.
 void ColoredPrintf(const XTestColor& color, const char* fmt, ...);
 
+// Prints a string containing code-encoded text.  The following escape sequences
+// can be used in the string to control the text color:
+//
+//   @@    prints a single '@' character.
+//   @R    changes the color to red.
+//   @G    changes the color to green.
+//   @Y    changes the color to yellow.
+//   @D    changes to the default terminal text color.
 void PrintColorEncoded(const char* str);
 }  // namespace internal
 }  // namespace xtest
